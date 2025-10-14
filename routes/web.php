6bfb31fe6692b36660bc896e9eab50f83b761295
enjelika83 <wayanenjelika@gmail.com>
2025-10-14 18:11:17 +0700

    wayan push perbaikan web.php

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route test environment (punya kamu)
+<<<<<<< HEAD
 Route::get('/test-env', function () {
     dd(env('DB_CONNECTION'));
 });
-
-// Route daftar layanan (dari remote)
+=======
+// Route daftar layanan
 Route::get('/daftar-layanan', [DaftarLayananController::class, 'index'])->name('daftar-layanan.index');
 Route::get('/daftar-layanan/create', [DaftarLayananController::class, 'create'])->name('daftar-layanan.create');
 Route::post('/daftar-layanan', [DaftarLayananController::class, 'store'])->name('daftar-layanan.store');
 Route::delete('/daftar-layanan/{id}', [DaftarLayananController::class, 'destroy'])->name('daftar-layanan.destroy');
+>>>>>>> e6f327a9a164c6e0f663da7f7993d837d672acd6
