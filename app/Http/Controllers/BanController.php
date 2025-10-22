9f4164702ba0fcd6dd49ad43b79a1d867c13698c
EsGus <subarukun65@gmail.com>
2025-10-22 08:37:05 +0700

    ganti nama control bagian gear sama ban by wayan

--- app/Http/Controllers/BanController.php
+++ app/Http/Controllers/BanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OliController extends Controller
+class BanController extends Controller
 {
     public function index()
     {
